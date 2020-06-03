feaf1a591b62f83c53bd71aa6daac5cc19a9a635
yejinlei <4010896@qq.com>
2020-06-03 15:52:22 +0800

    优化脚本

--- 自动机器学习/pre_env.sh
+++ 自动机器学习/pre_env.sh
@@ -1,7 +1,8 @@
 name=$1
 pyenv virtualenv 3.7.0 ${name}_3.7.0
+pyenv deactivate
+pyenv activate ${name}_3.7.0
 pyenv local ${name}_3.7.0
 pyenv rehash
-pyenv activate ${name}_3.7.0
-pip install -U pip
-
+pip install --upgrade pip
+pip install -r requirements.txt
